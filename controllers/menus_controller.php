ebded869c51394a068fc9ebe253cf76cfe3b231b
zpartakov <zpartakov@akademia.ch>
2012-12-19 12:35:35 +0100

    putz

---
+++ controllers/menus_controller.php
@@ -0,0 +1,113 @@
+<?php
+class MenusController extends AppController {
+
+	var $name = 'Menus';
+	var $components = array('Auth', 'RequestHandler');
+	function beforeFilter() {
+		$this->Auth->allow('index','indeximg', 'view', 'viewimg', 'commissions', 'lesmenus_saison');
+	}
+	function index() {
+		$this->Menu->recursive = 0;
+		$this->set('menus', $this->paginate());
+	}
+	function indeximg() {
+		$this->layout = 'facile';
+		
+		$this->Menu->recursive = 0;
+		$this->set('menus', $this->paginate());
+	}
+	
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid menu', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('menu', $this->Menu->read(null, $id));
+	}	
+	/*
+	 * facilitated view
+	 */
+	function viewimg($id = null) {
+		$this->layout = 'facile';
+		
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid menu', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('menu', $this->Menu->read(null, $id));
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			$this->Menu->create();
+			if ($this->Menu->save($this->data)) {
+				$this->Session->setFlash(__('The menu has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The menu could not be saved. Please, try again.', true));
+			}
+		}
+		$saisons = $this->Menu->Saison->find('list');
+		$recettes = $this->Menu->Recette->find('list');
+		$this->set(compact('saisons', 'recettes'));
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid menu', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->Menu->save($this->data)) {
+				$this->Session->setFlash(__('The menu has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The menu could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Menu->read(null, $id);
+		}
+		$saisons = $this->Menu->Saison->find('list');
+		$recettes = $this->Menu->Recette->find('list');
+		$this->set(compact('saisons', 'recettes'));
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for menu', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->Menu->delete($id)) {
+			$this->Session->setFlash(__('Menu deleted', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Menu was not deleted', true));
+		$this->redirect(array('action' => 'index'));
+	}
+	
+	/*
+	 * other functions
+	 */
+	
+	/*
+	 * print menus for a given season
+	 */
+	function lesmenus_saison($saison){
+		$sql="SELECT * FROM menus WHERE (saison_id='" .$saison ."' OR saison_id='5')";
+		//echo $sql;
+		$sql=mysql_query($sql);
+		$i=0;
+	echo "<h2>Menus proposés pour cette saison</h2>";
+	echo "<ul>";
+		while($i<mysql_num_rows($sql)){
+			echo "<li><a href=\"menus/view/" .mysql_result($sql,$i,'id')."\">" 
+			.mysql_result($sql,$i,'libelle')
+			."</a>";
+			echo "</li>";
+			$i++;
+		}
+	echo "</ul>";		
+	}
+	
+}
---
+++ views/pages/recettes.ctp
@@ -0,0 +1,54 @@
+<?php    
+$this->set("title_for_layout","Recettes");
+$largeur_image=150;
+/*
+			<li><a href="<? echo CHEMIN; ?>recettes/?typ=16">Céréales</a></li>
+			<li><a href="<? echo CHEMIN; ?>recettes/?typ=18">Légumes</a></li>
+ */
+?>
+<table>
+  <tr>
+    <td>
+	    <a href="<? echo CHEMIN; ?>recettes/indeximg/?typ=5" title="Recettes de légumes et salades">		
+		<? echo $html->image('navi/legumes_salades_t.jpg', array("alt"=>"Lien vers les recettes de légumes et salades", "width" => $largeur_image."px", "class" => "rounded", "style"=>"vertical-align: middle"));?>
+		</a>
+	</td>
+    <td>
+	    <a href="<? echo CHEMIN; ?>recettes/indeximg/?typ=4" title="Recettes de soupes et potages">		
+		<? echo $html->image('navi/potagesWP.jpg', array("alt"=>"Lien vers les recettes de soupes et potages", "width" => $largeur_image."px", "class" => "rounded", "style"=>"vertical-align: middle"));?>
+		</a>
+	</td>
+    <td>
+	    <a href="<? echo CHEMIN; ?>recettes/indeximg/?typ=9" title="Recettes de viandes">
+		<? echo $html->image('navi/viande_t.jpg', array("alt"=>"Lien vers les recettes de viandes", "width" => $largeur_image."px", "class" => "rounded", "style"=>"vertical-align: middle"));?>
+		</a>
+	</td>
+    <td>
+	    <a href="<? echo CHEMIN; ?>recettes/indeximg/?typ=8" title="Lien vers les recettes de poissons">
+		<? echo $html->image('navi/poisson_t.jpg', array("alt"=>"Lien vers les recettes de poissons", "width" => $largeur_image."px", "class" => "rounded", "style"=>"vertical-align: middle"));?>
+		</a>
+	</td>  
+	</tr>
+   <tr>
+
+    <td>
+    	<a href="<? echo CHEMIN; ?>recettes/indeximg/?typ=15" title="Recettes de pommes de terre, pâtes et céréales">
+		<? echo $html->image('navi/pdt_pasta_cer_t.jpg', array("alt"=>"Lien vers les recettes de pommes de terre, pâtes et céréales", "width" => $largeur_image."px", "class" => "rounded", "style"=>"vertical-align: middle"));?>
+		</a>
+    </td>
+    <td>
+	    <a href="<? echo CHEMIN; ?>recettes/indeximg/?typ=19" title="Recettes de fruits et desserts">
+		<? echo $html->image('navi/fruits_desserts_t.jpg', array("alt"=>"Lien vers les recettes de fruits et desserts", "width" => $largeur_image."px", 	"class" => "rounded", "style"=>"vertical-align: middle"));?>
+		</a>
+    </td>
+    <td>
+	    <a href="<? echo CHEMIN; ?>recettes/indeximg/?typ=20" title="Recettes de lait, fromage et oeufs">
+		<? echo $html->image('navi/lait_fromage_oeufs_t.jpg', array("alt"=>"Lien vers les recettes de lait, fromage et oeufs", "width" => $largeur_image."px", "class" => "rounded", "style"=>"vertical-align: middle"));?>
+		</a>
+    </td>
+        <td>&nbsp;
+    </td>
+  </tr>
+</table>
+
+	
